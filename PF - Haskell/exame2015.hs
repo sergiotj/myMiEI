23cddcb6ea21faad65d3dd005132d3be57c03081
Sérgio Oliveira <stoj97@gmail.com>
2018-01-28 22:54:24 +0000

    [PF] - fixed fodilhaaao

--- PF - Haskell/exame2015.hs
+++ PF - Haskell/exame2015.hs
@@ -13,7 +13,7 @@ type Nota = Float
 -- lista ordenada por numero de aluno
 
 exTurma :: TurmaL
-exTurma = [(77730,("Sergio",9.6)),(77870,("Vitor",10)),(77871,("Ricardo",11)),(77872,("Fernando",5.3)),(77873,("Bruno",4.2)),(77874,("Sara",15))]
+exTurma = [(77730,("Sergio",15)),(77870,("Vitor",15)),(77871,("Ricardo",11)),(77872,("Fernando",5.3)),(77873,("Bruno",4.2)),(77874,("Diana",15))]
 
 exAlunos :: [Aluno]
 exAlunos = [("Sergio",9.6),("Vitor",10),("Ricardo",11)]
@@ -34,14 +34,25 @@ melhorNota turma = aux turma 0 where
 	aux [] melhor = melhor
 	aux ((_,(_,nota)):xs) melhor = if (nota > melhor) then aux xs nota else aux xs melhor
 
-rMelhorNota :: TurmaL -> Float -> TurmaL
+rMelhorNota :: TurmaL -> Float -> (Numero,Aluno)
 rMelhorNota ((num,(nome,nota)):xs) n = if (nota == n) then (num,(nome,nota)) else rMelhorNota xs n
 
-tiraMelhorNota :: TurmaL -> TurmaL -> TurmaL
+tiraMelhorNota :: TurmaL -> (Numero,Aluno) -> TurmaL
 tiraMelhorNota [] _ = []
-tiraMelhorNota ((num,(nome,nota)):xs) (num1,(nome1,nota1):ys) = if (num == num1 && nome == nome1 && nota == nota1) then tiraMelhorNota xs else (num,(nome,nota)):tiraMelhorNota xs
+tiraMelhorNota ((num,(nome,nota)):xs) (num1,(nome1,nota1)) = if (num == num1 && nome == nome1 && nota == nota1) then tiraMelhorNota xs (num1,(nome1,nota1)) else
+	(num,(nome,nota)):tiraMelhorNota xs (num1,(nome1,nota1))
 
-join1 :: TurmaL -> TurmaL
-join1 [] = []
-join1 (x:xs) = (tiraMelhorNota (x:xs) (rMelhorNota (x:xs) (melhorNota (x:xs)))):join1 xs
+retiraMelhorNota :: TurmaL -> TurmaL
+retiraMelhorNota [] = []
+retiraMelhorNota (x:xs) = (tiraMelhorNota (x:xs) (rMelhorNota (x:xs) melhorNota1)) where
+	melhorNota1 = melhorNota (x:xs)
 
+jJoin :: TurmaL -> TurmaL
+jJoin [] = []
+jJoin (x:xs) = (rMelhorNota (x:xs) melhorNota1):jJoin (retiraMelhorNota (x:xs)) where
+	melhorNota1 = melhorNota (x:xs)
+
+top :: Int -> TurmaL -> [String]
+top n lista = aux n (jJoin lista) where
+	aux 0 lista = []
+	aux n ((num,(nome,nota)):xs) = nome:aux (n-1) xs
