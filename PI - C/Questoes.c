1ffcb366f3aee8ad0d7fcf00887e80601fbec4fb
Sérgio Oliveira <stoj97@gmail.com>
2018-02-22 10:34:01 +0000

    [PI] - minor fix

--- PI - C/Questoes.c
+++ PI - C/Questoes.c
@@ -3,6 +3,12 @@
 #include <string.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <math.h>
+
+typedef enum movimento {Norte, Oeste, Sul, Este} Movimento;
+typedef struct posicao {
+    int x, y;
+} Posicao;
 
 /*
 
